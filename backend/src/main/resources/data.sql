2f1ba7a1c621999786f1c07a9f9bbc043aa70944
yuqisun <7748145+yuqisun@users.noreply.github.com>
2024-05-09 21:11:42 +0800

    Update data.sql
    adjust code

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,10 +1,10 @@
-insert into Voter(email,phone,birthDate,gender,citizenUS,age18,city,firstName,lastName,streetAddress1,streetAddress2,zipCode,socialSecurity) values ('Jordan@google.com', '111-111-1111','01/01/2000','male',true,true,'Chicago','Jordan','Michael','XXXX Street','YYYY Building', '000111', '111111XXXX');
-insert into Voter(email,phone,birthDate,gender,citizenUS,age18,city,firstName,lastName,streetAddress1,streetAddress2,zipCode,socialSecurity) values ('Iverson@google.com', '222-222-2222','01/01/2000','male',true,true,'Philadelphia','Iverson','Allen','XXXX Street','YYYY Building', '000111', '222222XXXX');
-insert into Voter(email,phone,birthDate,gender,citizenUS,age18,city,firstName,lastName,streetAddress1,streetAddress2,zipCode,socialSecurity) values ('Kobe@google.com', '333-333-3333','01/01/2000','male',true,true,'LosAngeles','Kobe','Bryant','XXXX Street','YYYY Building', '000111', '333333XXXX');
+insert into Voter(email,phone,birthDate,gender,citizenUS,age18,city,firstName,lastName,streetAddress1,streetAddress2,zipCode,socialSecurity) values ('Jordan@mock.com', '111-111-1111','01/01/2000','male',true,true,'Chicago','Jordan','Michael','XXXX Street','YYYY Building', '000111', '111111XXXX');
+insert into Voter(email,phone,birthDate,gender,citizenUS,age18,city,firstName,lastName,streetAddress1,streetAddress2,zipCode,socialSecurity) values ('Iverson@mock.com', '222-222-2222','01/01/2000','male',true,true,'Philadelphia','Iverson','Allen','XXXX Street','YYYY Building', '000111', '222222XXXX');
+insert into Voter(email,phone,birthDate,gender,citizenUS,age18,city,firstName,lastName,streetAddress1,streetAddress2,zipCode,socialSecurity) values ('Kobe@mock.com', '333-333-3333','01/01/2000','male',true,true,'LosAngeles','Kobe','Bryant','XXXX Street','YYYY Building', '000111', '333333XXXX');
 
-insert into Authentication(id, email, password) values (1, 'Jordan@google.com', '11111111');
-insert into Authentication(id, email, password) values (2, 'Iverson@google.com', '22222222');
-insert into Authentication(id, email, password) values (3, 'Kobe@google.com', '33333333');
+insert into Authentication(id, email, password) values (1, 'Jordan@mock.com', '11111111');
+insert into Authentication(id, email, password) values (2, 'Iverson@mock.com', '22222222');
+insert into Authentication(id, email, password) values (3, 'Kobe@mock.com', '33333333');
 
 insert into RegisterStatus(id, status) values (1, 'inprogress');
 insert into RegisterStatus(id, status) values (2, 'completed');
